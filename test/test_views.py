77619cc73292e5543b8f7e8dc09ba525fba5c88b
piotr67 <piotrgorszkowski@gmail.com>
2017-04-08 13:10:03 +0200

    test/test_views.py

--- test/test_views.py
+++ test/test_views.py
@@ -14,5 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        print rv.data
         self.assertEquals('{ "imie": "Piotr", "mgs": "Witaj swiecie"}', rv.data)
